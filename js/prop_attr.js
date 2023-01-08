a2bf3daa9df3ded3a94b5600e69a02f4352cc91d
elle1418 <elcua0614@gmail.com>
2023-01-08 10:02:37 +0900

    リスト4-6~4-10

---
+++ js/prop_attr.js
@@ -0,0 +1,5 @@
+Vue.createApp({})
+  .component('my-hello', {
+    template: `<div title="reslut" class="main" id="hello2">こんにちは、Vue.js！</div>`
+  })
+  .mount('#app');
---
+++ js/prop_basic.js
@@ -0,0 +1,6 @@
+Vue.createApp({})
+  .component('my-hello', {
+    props: [ 'yourName' ],
+    template: `<div>こんにちは、{{ yourName }}さん</div>`,
+  })
+  .mount('#app');
