f5367da865de48f8f45acd825b09a4ceedb7dd7f
mitani1993 <fytim55@gmail.com>
2023-01-15 09:30:45 +0900

    propsの記述方法

--- js/prop_basic.js
+++ js/prop_basic.js
@@ -1,6 +1,27 @@
 Vue.createApp({})
-  .component('my-hello', {
-    props: [ 'yourName' ],
-    template: `<div>こんにちは、{{ yourName }}さん</div>`,
-  })
-  .mount('#app');
+    .component('my-hello', {
+        // props: [ 'yourName' ],
+        props: {
+            yourName: {
+                type: String,
+                required: true,
+                validator(value) {
+                    return value.length <= 5;
+                }
+            }
+        },
+        // props: {
+        //     yourName: [String, Number]
+        // },
+        // props: {
+        //     user: User
+        // },
+        // props: {
+        //     yourName: {
+        //         type: String,
+        //         default: '名無権兵衛'
+        //     }
+        // },
+        template: `<div>こんにちは、{{ yourName }}さん</div>`,
+    })
+    .mount('#app');
