0918c32f46fe84b43079abe26ad4176d532b3423
elle1418 <elcua0614@gmail.com>
2023-01-15 09:57:42 +0900

    リスト4-19

--- js/emit_basic.js
+++ js/emit_basic.js
@@ -7,12 +7,25 @@ Vue.createApp({
     methods: {
         onplus(e) {
             this.current += e;
+        },
+        onmounted(e) {
+          console.log(e);
         }
     }
 })
     .component('my-counter', {
         props: ['step'],
-        emits: ['plusMinus'],
+        // emits: ['plusMinus'],
+        emits: {
+          plusMinus(step) {
+            if (step && Number.isInteger(step)) {
+              return true;
+            } else {
+              console.log('plusMinus event object must be integer.');
+              return false;
+            }
+          }
+        }, 
         template: `<button type="button" v-on:click="onclick">
 {{ step }}</button>`,
         methods: {
