d7d1af488cf2d62364460d37aec70e4e1b696c34
mitani1993 <fytim55@gmail.com>
2023-03-05 09:40:47 +0900

    リスト5-11

---
+++ js/teleport_child.js
@@ -0,0 +1,41 @@
+Vue
+.createApp({})
+.component('my-teleport', {
+  data() {
+    return {
+      show: false
+    };
+  },
+  template: `
+      <form>
+        <input type="button" v-on:click="onclick(true)"
+          value="ダイアログを開く" />
+      </form>
+      <teleport to="#popup">
+        <my-teleport-child msg="独自のダイアログです。" />
+      </teleport>
+  `,
+  methods: {
+    onclick(flag) {
+      this.show = flag;
+    }
+  },
+  provide() {
+    return {
+      show: Vue.computed(() => this.show),
+      onclick: this.onclick
+    };
+  }
+})
+.component('my-teleport-child', {
+  props: { msg: String },
+  // 親コンポーネントのprovide値を取得
+  inject: ['show', 'onclick'],
+  template:`
+    <div id="my-dialog" class="dialog" v-if="show.value">
+      <p>{{ msg }}</p>
+      <input type="button" v-on:click="onclick(false)" value="閉じる" />
+    </div>
+  `
+})
+.mount('#app');
