c42952ff3f9f75796976ac66701b56774aa62240
mitani1993 <fytim55@gmail.com>
2023-05-20 09:55:11 +0900

    リスト 5-58 efineCustomElementメソッド

--- js/my-hover.js
+++ js/my-hover.js
@@ -1,22 +1,44 @@
-class MyHover extends HTMLElement {
-  constructor() {
-    super();
-    // shadow DOMを準備
-    const shadow = this.attachShadow({mode: 'open'});
-    // <img>要素を作成
-    const root = document.createElement('img');
-    root.setAttribute('src', this.getAttribute('leave-src'));
-    // イベントリスナーを準備
-    root.addEventListener('mouseenter', e => {
-      root.setAttribute('src', this.getAttribute('enter-src'));
-    });
-    root.addEventListener('mouseleave', e => {
-      root.setAttribute('src', this.getAttribute('leave-src'));
-    });
-    // <img>要素をShadow DOMに追加
-    shadow.appendChild(root);
-  }
-}
+// class MyHover extends HTMLElement {
+//   constructor() {
+//     super();
+//     // shadow DOMを準備
+//     const shadow = this.attachShadow({mode: 'open'});
+//     // <img>要素を作成
+//     const root = document.createElement('img');
+//     root.setAttribute('src', this.getAttribute('leave-src'));
+//     // イベントリスナーを準備
+//     root.addEventListener('mouseenter', e => {
+//       root.setAttribute('src', this.getAttribute('enter-src'));
+//     });
+//     root.addEventListener('mouseleave', e => {
+//       root.setAttribute('src', this.getAttribute('leave-src'));
+//     });
+//     // <img>要素をShadow DOMに追加
+//     shadow.appendChild(root);
+//   }
+// }
+
+// defineCustomElementメソッドを使った場合
+const MyHover = Vue.defineCustomElement({
+  data() {
+    return {
+      src: this.leaveSrc
+    };
+  },
+  props: {
+    leaveSrc: String,
+    enterSrc: String
+  },
+  methods: {
+    onenter() {
+      this.src = this.enterSrc;
+    },
+    onleave() {
+      this.src = this.leaveSrc;
+    },
+  },
+  template: `<img v-bind:src="src" v-on:mouseenter="onenter" v-on:mouseleave="onleave" />`,
+});
 
 // <my-hover>要素を登録
 customElements.define('my-hover', MyHover);
