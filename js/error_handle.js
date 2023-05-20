55a3cb227a6857233e5c7edd70b7c717e1b35a00
mitani1993 <fytim55@gmail.com>
2023-05-20 09:11:31 +0900

    リスト 5-50 error_handler

---
+++ js/error_handle.js
@@ -0,0 +1,31 @@
+const app = Vue
+  .createApp({})
+  // 親子関係にあるmy-parent/my-childコンポーネントを定義
+  .component('my-parent', {
+    template: `
+      <div id="parent">
+        <my-child />
+      </div>
+    `
+  })
+  .component('my-child', {
+    // マウント時に無条件に例外をスロー
+    mounted() {
+      throw new Error('Error is occured by my-child')
+    },
+    template: `
+      <div id="child">
+        MyChild
+      </div>
+    `
+  });
+
+// エラーハンドラーを定義
+app.config.errorHandler = (error, vm, info) => {
+  console.log('■■ Global ■■');
+  console.log(error);
+  console.log(vm);
+  console.log(info);
+};
+
+app.mount('#app');
