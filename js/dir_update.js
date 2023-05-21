500e55e0476324561936635dd27876f2a4bd3e71
mitani1993 <fytim55@gmail.com>
2023-05-22 08:20:55 +0900

    リスト 6-25 mounted とupdatedをまとめる

--- js/dir_update.js
+++ js/dir_update.js
@@ -6,14 +6,17 @@ Vue
       };
     }
   })
-  .directive('highlight', {
-    // 紐づいたときの処理(初回のみ)
-    mounted(el, binding, vnode, prevNode) {
-      el.style.backgroundColor = binding.value;
-    },
-    // 上位コンポーネントが変化したとき
-    updated(el, binding, vnode, prevNode) {
-      el.style.backgroundColor = binding.value;
-    }
+  // .directive('highlight', {
+  //   // 紐づいたときの処理(初回のみ)
+  //   mounted(el, binding, vnode, prevNode) {
+  //     el.style.backgroundColor = binding.value;
+  //   },
+  //   // 上位コンポーネントが変化したとき
+  //   updated(el, binding, vnode, prevNode) {
+  //     el.style.backgroundColor = binding.value;
+  //   }
+  // })
+  .directive('highlight', (el, binding, vnode, prevNode) => {
+    el.style.backgroundColor = binding.value;
   })
   .mount('#app');
