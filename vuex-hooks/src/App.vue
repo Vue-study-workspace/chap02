fab3e20c2b24b932cbccff30b58598a9b301438c
mitani1993 <fytim55@gmail.com>
2023-05-24 08:33:19 +0900

    リスト 9-7 store getter

--- vuex-hooks/src/App.vue
+++ vuex-hooks/src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p>書籍は全部で{{ booksCount }}冊あります。</p>
+    <ul v-for="b of getBooksByPrice(2500)" v-bind:key="b.isbn">
+      <li>{{ b.title }}（{{ b.price }}円）<br />ISBN：{{ b.isbn }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  computed: mapGetters(['booksCount', 'getBooksByPrice']),
 }
 </script>
 
--- vuex-hooks/src/store/index.js
+++ vuex-hooks/src/store/index.js
@@ -1,9 +1,48 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  strict: true,
   state: {
+    books: [
+      {
+        isbn: '978-4-2960-8009-0',
+        title: '作って楽しむプログラミング Androidアプリ超入門',
+        price: 2200
+      },
+      {
+        isbn: '978-4-7981-6849-4',
+        title: '独習Ruby 新版',
+        price: 3740
+      },
+      {
+        isbn: '978-4-7741-9763-0',
+        title: '独習PHP 第4版',
+        price: 2480
+      },
+      {
+        isbn: '978-4-7981-6956-9',
+        title: '基礎から学ぶReact Native入門',
+        price: 1760
+      },
+      {
+        isbn: '978-4-297-11496-1',
+        title: 'Javaポケットリファレンス',
+        price: 3278
+      },
+    ]
   },
   getters: {
+    booksCount(state) {
+      return state.books.length;
+    },
+    getBooksByPrice(state) {
+      return price => {
+        return state.books.filter(book => book.price < price);
+      };
+    },
+    booksCount3000(state, getters) {
+      return getters.getBooksByPrice(3000).length;
+    }
   },
   mutations: {
   },
